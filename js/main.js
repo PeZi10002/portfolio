68c5dee021f3b08a84cc358b95e224832b115abe
pezi <pezi10002@hotmail.com>
2020-06-15 20:33:33 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ var alter = calculate_age(new Date(1990, 3, 26));
 
 biotext.innerHTML +=
   alter +
-  " Jahre alt. Nach 12 Jahren als hauptberuflicher Musiker, sehe ich " +
+  " Jahre alt. Nach 12 Jahren als hauptberuflicher Musiker sehe ich " +
   "meine berufliche Zukunft nun als Programmierer. Ich studiere zur Zeit am IT-Kolleg Imst und " +
-  "möchte mich auf die Webentwicklung spezialisieren. Ansonsten bewege ich mich gern in der freien Natur und begeistere mich für Sportarten" +
+  "möchte mich auf die Webentwicklung spezialisieren. In meiner Freizeit bewege ich mich gern in der freien Natur und begeistere mich für Sportarten" +
   " wie Radfahren, Joggen und Darts.";
